978eee94fcecd79f36c60f2b59c80c577cdc64af
SeVen <nshusa99@gmail.com>
2016-01-27 14:43:39 -0600

    Enumerated DialogueOption types.

--- src/main/astraeus/content/dialogue/DialogueOption.java
+++ src/main/astraeus/content/dialogue/DialogueOption.java
@@ -2,24 +2,53 @@ package main.astraeus.content.dialogue;
 
 import main.astraeus.game.model.entity.mobile.player.Player;
 
+/**
+ * The class responsible for options within dialogues.
+ * 
+ * @author SeVen
+ */
 public abstract class DialogueOption {
 
-	public static final int FIRST_OPTION = 2471;
+  /**
+   * The type of dialogue option.
+   */
+  public enum OptionType {
+    /**
+     * The first line of a dialogue option.
+     */
+    FIRST_OPTION,
 
-	public static final int SECOND_OPTION = 2472;
+    /**
+     * The second line of a dialogue option.
+     */
+    SECOND_OPTION,
 
-	public static final int THIRD_OPTION = 2473;
+    /**
+     * The first line of a dialogue option.
+     */
+    THIRD_OPTION,
 
-	/**
-	 * Handles a players option dialogue.
-	 * 
-	 * @param player
-	 *            The player to handle this option dialogue for.
-	 * 
-	 * @param button
-	 *            The button to click on the option dialogue.
-	 * 
-	 * @return {@code true} if this can be performed, {@code false} otherwise.
-	 */
-	public abstract boolean handleSelection(Player player, int buttonId);
+    /**
+     * The fourth line of a dialogue option.
+     */
+    FOURTH_OPTION,
+
+    /**
+     * The fifth line of a dialogue option.
+     */
+    FIFTH_OPTION
+  }
+
+  /**
+   * Handles a players option dialogue.
+   * 
+   * @param player
+   *            The player to handle this option dialogue for.
+   * 
+   * @param button
+   *            The button to click on the option dialogue.
+   * 
+   * @return {@code true} if this can be performed, {@code false} otherwise.
+   */
+  public abstract boolean handleSelection(Player player, OptionType option);
 }
